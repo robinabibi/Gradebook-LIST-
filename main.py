94127583b4b3aaf9eab87101442a1e87970ea956
robina bibi <>
2022-09-30 20:27:48 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+subjects = ["physics","calculus","poetry","history,"]
+print(subjects)
+grades=[98,97,85,88]
+print(grades)
+gradebook=[subjects,grades]
+print(gradebook)
+gradebook = (("Computer Science",100))
+print(gradebook)
+subjects.append(["Computer Science",100])
+print(subjects)
+subjects.append(("Visual Arts",93))
+print(subjects)
+subjects = (["physics",98,"calculus",97,"poetry",85,"history",88,"Computer Science",100,"Visual Arts",93])
+print(subjects)
+subjects.append('pass')
+print(subjects)
+full_grade_book = (['gradeboook, last_semester_gradebook'])
+print(full_grade_book)
